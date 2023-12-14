c0d275951dc01b31d83babbd36bc8fdef7c2de59
Leela Aurelia <git@elia.garden>
2023-12-14 23:52:04 +0000

    nix: finally fix RA

--- shell.nix
+++ shell.nix
@@ -1,19 +1,21 @@
 let 
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
   pkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  rustChannel = (pkgs.rustChannelOf { rustToolchain = ./rust-toolchain.toml; });
   packages = with pkgs; [
     pkg-config
     udev
     alsa-lib
     gtk3
-    ((rustChannelOf { rustToolchain = ./rust-toolchain.toml; }).rust.override (old:
-      { extensions = ["rust-src" "rust-analysis"]; }))
+    rustChannel.rust
+    rustChannel.rust-src
   ];
 in
   pkgs.mkShell {
     buildInputs = packages;
     shellHook = ''
       export LD_LIBRARY_PATH="/usr/lib:${pkgs.lib.makeLibraryPath packages}:''${LD_LIBRARY_PATH}"	
+      export RUST_SRC_PATH="${rustChannel.rust-src}/lib/rustlib/src/rust/library"
     '';
 
     RUST_BACKTRACE = 1;
